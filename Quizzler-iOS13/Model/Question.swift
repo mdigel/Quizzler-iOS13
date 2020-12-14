e81e1d258b14cccdfd0464301a9baf4f3d18d715
Matt Digel <med5342@gmail.com>
2020-12-13 18:50:57 -0800

    True and false quiz complete

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Matthew Digel on 12/13/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+    
+}
